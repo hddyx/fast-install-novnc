0f6c6c51744c51acf55d59a17a8c75053301c83e
hddyx <166891577+hddyx@users.noreply.github.com>
2025-01-25 18:29:12 +0800

    更新 install.sh
    update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,4 @@ cd /
 git clone https://github.com/novnc/noVNC.git
 sudo chmod +755 /noVNC/utils/novnc_proxy
 cd /noVNC/utils
-./novnc_proxy
-sudo iptables -I INPUT -p tcp --dport 6080 -j ACCEPT
-sudo iptables-save
+./novnc_proxy --listen 5900
